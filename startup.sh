5e8313a0be9c1046635ce34e808d5af70d4f17a1
吉澤 直明 <yossa1017@hotmail.com>
2021-11-14 18:17:38 +0900

    不要ファイル削除

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#Dockerfileでは複数コマンド実行できないためにシェルで複数コマンド実行。
-cd /workspace/ai-app-for-docker
-streamlit run streamlit.py
-#streamlit run streamlit.py --server.enableCORS=false
-#streamlit run streamlit.py --server.enableWebsocketCompression=false
-#streamlit run my_app.py --server.enableXsrfProtection=false
